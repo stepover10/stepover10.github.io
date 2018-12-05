3260c5c34cb3abf64689bd3527742a90ac890dbf
stepover10 <stepover10@gmail.com>
2018-12-05 15:38:54 +0900

    test

--- src/root/root.js
+++ src/root/root.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { hashHistory } from 'react-router'
 import App from 'shared/app';
 
 const Root = () => (
-    <HashRouter>
+    <BrowserRouter history={hashHistory} basename={process.env.PUBLIC_URL}>
         <App />
-    </HashRouter>
+    </BrowserRouter>
 );
 
 export default Root;
