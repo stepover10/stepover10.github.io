de7986cbd957d1e67f85511625e163699bc106f7
stepover10 <stepover10@gmail.com>
2018-12-05 15:41:52 +0900

    test

--- src/index/index.js
+++ src/index/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Root from 'root/root'
-import registerServiceWorker from 'registerServiceWorker/registerServiceWorker'
+// import registerServiceWorker from 'registerServiceWorker/registerServiceWorker'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
-registerServiceWorker();
+// registerServiceWorker();
--- src/root/root.js
+++ src/root/root.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { hashHistory } from 'react-router'
 import App from 'shared/app';
 
 const Root = () => (
