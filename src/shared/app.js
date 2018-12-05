e6b103b2d9f031ba33c155018dfe1c5dadab0168
stepover10 <stepover10@gmail.com>
2018-12-05 15:53:05 +0900

    test

--- src/shared/app.js
+++ src/shared/app.js
@@ -5,12 +5,10 @@ import { Profile, Work } from 'pages';
 export default class App extends Component {
     render() {
         return (
-            <HashRouter>
               <Switch>
                   <Route exact path="/" component={Profile} />
-                  <Route path="/work" component={Work} />
-              </Switch>
-            </HashRouter>
+                  <Route exact path="/work" component={Work} />
+              </Switch>        
         );
     }
 }
