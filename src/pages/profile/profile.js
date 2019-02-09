fffefe8df898116a53e6cf43e32d5cc35891eeec
stepover10 <stepover10@gmail.com>
2019-02-09 19:05:53 +0900

    최신화 테스트

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -4,71 +4,71 @@ import PrHello from './components/PrHello';
 import PrExp from './components/PrExp';
 
 export default class Profile extends Component {
+  componentDidMount() {
+    this.moveBody();
+    this.menuAct();
+  }
 
-    componentDidMount(){      
-      this.moveBody()
-      this.menuAct()
-    }
+  menuAct() {
+    const aDom = document.getElementById("me-menu")
+      .querySelectorAll('a');
+    const actStatus = (event) => {
+      aDom.forEach(elm => {
+        elm.classList.remove('act');
+      });
+      event.target.classList.add('act');
+    };
 
-    menuAct(){      
-      const aDom = document.getElementById("me-menu").querySelectorAll('a')
-      const actStatus = ( event ) => {
-        aDom.forEach( elm => {
-          elm.classList.remove('act')
-        })
-        event.target.classList.add('act')
-      }
-      
-      aDom.forEach( elm => {        
-        elm.addEventListener("click", actStatus );
-      })
-    }
+    aDom.forEach(elm => {
+      elm.addEventListener("click", actStatus);
+    });
+  }
 
-    moveBody(){    
-      document.querySelectorAll('.scrollLink').forEach( anchor => {
+  moveBody() {
+    document.querySelectorAll('.scrollLink')
+      .forEach(anchor => {
         anchor.addEventListener('click', function (event) {
-            event.preventDefault()        
-            document.getElementById( this.getAttribute('data-href') ).scrollIntoView({
-               block: 'start',  
-               behavior: 'smooth'
-            })
-        })
-      })
-    }
+          event.preventDefault();
+          document.getElementById(this.getAttribute('data-href'))
+            .scrollIntoView({
+              block: 'start',
+              behavior: 'smooth'
+            });
+        });
+      });
+  }
 
-    render() {
-      return(
-          <div id="profileWrap">
-            <div className="body-left">
-                <div className="sticky-ly">
-                  <h1>ME<span>.</span></h1>
-                  <b />                  
-                  <ul id="me-menu" className="me-menu">
-                    <li><a data-href="hello_archor" className="scrollLink act">Intro</a></li>
-                    <li><a data-href="exp_archor" className="scrollLink">Experience</a></li>
-                    <li><a data-href="#">Skills</a></li>
-                    <li><a data-href="#">Resume</a></li>
-                  </ul>
-                </div>
-            </div>
-            
-            <div className="body-right">
-
-              {/* 인사말 */}
-              <PrHello arthor="hello_archor"/>
-
-              {/* 학교, 경력 */}
-              <PrExp arthor="exp_archor"/>
-
-              {/* 보유 스킬 */}
-
-
-              {/* 이력서 다운 */}
-
-            </div>   
+  render() {
+    return (
+      <div id="profileWrap">
+        <div className="body-left">
+          <div className="sticky-ly">
+            <h1>ME<span>.</span></h1>
+            <b/>
+            <ul id="me-menu" className="me-menu">
+              <li><a data-href="hello_archor" className="scrollLink act">Intro</a></li>
+              <li><a data-href="exp_archor" className="scrollLink">Experience</a></li>
+              <li><a data-href="#">Skills</a></li>
+              <li><a data-href="#">Resume</a></li>
+            </ul>
           </div>
-      )
-    }
+        </div>
+
+        <div className="body-right">
+
+          {/* 인사말 */}
+          <PrHello arthor="hello_archor"/>
+
+          {/* 학교, 경력 */}
+          <PrExp arthor="exp_archor"/>
+
+          {/* 보유 스킬 */}
+
+
+          {/* 이력서 다운 */}
+
+        </div>
+      </div>
+    );
+  }
 }
-
-
